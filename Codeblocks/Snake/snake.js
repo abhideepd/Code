168f4c8f4c31a628151ca952bc6cc3441227fde5
abhideepd <abhideepd@gmail.com>
2021-02-20 18:33:51 +0530

    msi

--- Codeblocks/Snake/snake.js
+++ Codeblocks/Snake/snake.js
@@ -1,39 +1,93 @@
 
 function init()
 {
-    main_canvas = document.getElementById("main_canvas");
-    main_canvas.width = main_canvas.height = 2000;
-    main_canvas.margin = '10px';
-    main_canvas.border = '1px solid black'
-    
-    rect = {
-        x:10,
-        y:10,
-        w:90,
-        h:90,
-        speed:10,
+    canvas = {
+        position : {
+            start: {
+                x: 1,
+                y: 1,
+            },
+        },
+        w: 2500,
+        h: 2500,
+        margin: '10px',
+        border: '1px solid black',
     }
+
+    game = {
+        status : 'start',
+        session: '',
+        speed: 10,
+    }
+
+    main_canvas = document.getElementById("main_canvas");
+    main_canvas.width = canvas.w;
+    main_canvas.height = canvas.h;
+    //main_canvas.margin = canvas.margin;
+    //main_canvas.border = canvas.border;
+    
+    snake = {
+        cells: [],
+        direction: 1,
+        initial_size: 5,
+        cs: 100,    // single cell size
+        x: canvas.position.start.x,
+        y: canvas.position.start.y,
+
+        create:function(){
+            for(var i=0; i<this.initial_size; i++)
+            {
+                this.cells.push({x: canvas.position.start.x+this.cs*i, y: canvas.position.start.y});
+            }
+        },
+        draw:function(){
+            for(var i=0; i<this.cells.length; i++)
+            {
+                pen.fillRect(this.cells[i].x, this.cells[i].y, this.cs, this.cs);
+            }
+        },
+        update:function(){
+            //initial_x = this.cells[0].x;
+            initial_y = this.cells[0].y;
+            for(var i=0; i<this.cells.length; i++)
+            {
+                //pen.clearRect(this.cells[i].x, this.cells[i].y, snake.cs, snake.cs);
+                this.cells[i]=({x: (this.cells[i].x+game.speed*this.direction), y:(initial_y)});
+            }
+        }
+    }
+    
     pen = main_canvas.getContext('2d');
     pen.fillStyle = "red";
-    
+    snake.create();
+    snake.draw();
 }
 function draw()
 {
-    pen.fillRect(rect.x, rect.y, rect.w, rect.h);
+    snake.draw();
 }
 function update()
 {
-    rect.x += rect.speed;
+    //if(snake.cells[snake.cells.length-1].x>canvas.w)
+      //  snake.direction = -1;
+    
+    //if(snake.cells[0].x<canvas.position.start)
+      //  snake.direction = 1;
+
+    snake.update();
 }
 function gameloop()
 {
     draw();
     update();
-    setInterval(draw, 100);
 }
 function start()
 {
-     
+    game.session = setInterval(gameloop, 100);
+}
+function stop()
+{
+    clearInterval(game.session);
 }
 init();
-gameloop();
+start();
