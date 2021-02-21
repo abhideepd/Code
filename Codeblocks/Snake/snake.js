c0c282b4e9c638b4c2ed3cdb66533c3eb485ea5b
abhideepd <abhideepd@gmail.com>
2021-02-22 01:01:18 +0530

    lenevo

--- Codeblocks/Snake/snake.js
+++ Codeblocks/Snake/snake.js
@@ -24,23 +24,36 @@ function init()
         speed: 120,
     }
 
-    canvas.position.end.x = canvas.w - canvas.position.start.x;
-    canvas.position.end.y = canvas.w - canvas.position.start.y;
+    canvas.position.end.x = 2500;
+    canvas.position.end.y = 2500;
     main_canvas = document.getElementById("main_canvas");
     main_canvas.width = canvas.w;
     main_canvas.height = canvas.h;
     main_canvas.style.margin = canvas.margin;
     main_canvas.style.border = canvas.border;
+    snakke_cell_size = 100;
+
+    food={
+        x:0,
+        y:0,
+        getRandom:function(){
+            this.x=Math.round(Math.random()*(canvas.w-snakke_cell_size)/snakke_cell_size);
+            this.y=Math.round(Math.random()*(canvas.w-snakke_cell_size)/snakke_cell_size);
+        },
+        draw:function(){
+            pen.fillRect(this.x, this.y, snakke_cell_size-2, snakke_cell_size-2);
+        },
+    }
     
     snake = {
-        head:{
-            x:canvas.position.start.x,
-            y:canvas.position.start.y,
-        },
         cells: [],
         direction: 'right',
         initial_size: 5,
-        cs: 100,    // single cell size
+        head:{
+            x:canvas.position.start.x+1,
+            y:canvas.position.start.y+1,
+        },
+        cs: snakke_cell_size,    // single cell size
         x: canvas.position.start.x,
         y: canvas.position.start.y,
 
@@ -57,40 +70,50 @@ function init()
             }
         },
         update:function(){
+
+            var cons = this.cs;
             var headX = this.cells[this.cells.length-1].x;
             var headY = this.cells[this.cells.length-1].y;
             var X = headX;
             var Y = headY;
-            var cons = this.cs;
             this.cells.shift();
+            
             if(this.direction=='right')
             {
                 X = X + cons;
-                console.log("update:function() -- 'right'");
+
+                //console.log("update:function() -- 'right'");
+                //console.log(this.cells);
             }
             else if(this.direction=='left')
             {
                 X = X - cons;
-                console.log("update:function() -- 'left'");
+
+                //console.log("update:function() -- 'left'");
+                //console.log(this.cells);
             }
             else if(this.direction=='down')
             {
-                Y = Y + cons;   
-                console.log("update:function() -- 'down'");
+                Y = Y + cons;
+                
+                //console.log("update:function() -- 'down'");
+                //console.log(this.cells);
             }
             else if(this.direction=='up')
             {
                 Y = Y - cons;
-                console.log("update:function() -- 'up'");
-            }
 
+                //console.log("update:function() -- 'up'");
+                //console.log(this.cells);
+            }
+            if((X==food.x)&&(Y==food.y))
+                 alert("found");
             this.head.x = X;
             this.head.y = Y;
-            console.log("headX: "+this.head.x+"  headY:"+this.head.y);
             this.cells.push({x:X, y:Y});
+            //console.log("headX: "+this.head.x+"  headY:"+this.head.y);
         }
-    }
-    
+    }    
     pen = main_canvas.getContext('2d');
     pen.fillStyle = "red";
     snake.create();
@@ -100,71 +123,44 @@ function draw()
 {
     pen.clearRect(0, 0, canvas.w, canvas.h);
     snake.draw();
+    food.draw();
 }
 function update()
 {   
     //  Boundry conditionas
-    var cons = snake.cs;
-    if(snake.head.x>=canvas.position.end.x)
+    if(snake.head.x>canvas.position.end.x)
     {
-        console.log("function update() -- 'left'");
-        /*var headX = snake.cells[0].x;
-        var headY = snake.cells[0].y;
-        X = headX - cons;
-        Y = headY;
-        snake.head.x = X;
-        snake.head.y = Y;
-        snake.cells.pop();
-        snake.cells.unshift({x:X, y:Y});*/
+        //console.log("function update() -- 'left'");
+        //console.log(snake.cells);
+        //snake.cells.push({x:(snake.cells[0].x-snake.cs), y:snake.cells[0].y});
+        //alert("XYZ");
         snake.direction = 'left';
     }
 
-    else if(snake.head.x<=canvas.position.start.x)
+    else if(snake.head.x<canvas.position.start.x-snake.cs)
     {
-        console.log("function update() -- 'right'");
-        /*var headX = snake.cells[snake.cells.length-1].x;
-        var headY = snake.cells[snake.cells.length-1].y;
-        X = headX + cons;
-        Y=headY;
-        snake.head.x = X;
-        snake.head.y = Y;
-        snake.cells.shift();
-        snake.cells.push({x:X, y:Y});*/
+        //console.log("function update() -- 'right'");
+        //console.log(snake.cells);
+        //alert("right");
         snake.direction = 'right';
     }
 
-    else if(snake.head.y>=canvas.position.end.y)
+    else if(snake.head.y>canvas.position.end.y)
     {
-        console.log("function update() -- 'up'");
-        /*var headX = snake.cells[0].x;
-        var headY = snake.cells[0].y;
-        X = headX;
-        Y = headY - cons;
-        snake.head.x = X;
-        snake.head.y = Y;
-        snake.cells.pop();
-        snake.cells.unshift({x:X, y:Y});*/
+        //console.log("function update() -- 'up'");
+        //console.log(snake.cells);
+        //alert("XYZ");
         snake.direction = 'up';
     }
     
-    else if(snake.head.y<=canvas.position.start.y)
+    else if(snake.head.y<canvas.position.start.y)
     {
-        console.log("function update() -- 'down'");
-        /*var headX = snake.cells[snake.cells.length-1].x;
-        var headY = snake.cells[snake.cells.length-1].y;
-        X = headX;
-        Y = headY + cons;
-        snake.head.x = X;
-        snake.head.y = Y;
-        snake.cells.shift();
-        snake.cells.push({x:X, y:Y});*/
+        //console.log("function update() -- 'down'");
+        //console.log(snake.cells);
+        //alert("XYZ");
         snake.direction = 'down';
     }
-
-    //else
-    //{
-        snake.update();
-    //}
+    snake.update();
 }
 function gameloop()
 {
@@ -178,8 +174,8 @@ function start()
     function keyPressed(e)
     {
         //console.log(e.key);
-        snake.direction = e.key.substring(5, e.key.length).toLowerCase();
-        //console.log(snake.direction);
+        var direction = e.key.substring(5, e.key.length).toLowerCase();
+        snake.direction = direction;
     }
     xyz=document.addEventListener('keydown', keyPressed);
 }
@@ -187,5 +183,19 @@ function stop()
 {
     clearInterval(game.session);
 }
+function operation_button(state, id)
+{
+    if(state=='Stop')
+    {
+        document.getElementById(id).value='Start';
+        stop();
+    }
+    else if(state=='Start')
+    {
+        document.getElementById(id).value='Stop';
+        start();
+    }
+}
 init();
+food.getRandom();
 start();
