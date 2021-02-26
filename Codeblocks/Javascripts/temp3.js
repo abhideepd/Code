3f627727a2c59ccdf6bfd39c7f1a950444a99382
abhideepd <abhideepd@gmail.com>
2021-02-27 00:18:56 +0530

    lenevo

--- Codeblocks/Javascripts/temp3.js
+++ Codeblocks/Javascripts/temp3.js
@@ -1,4 +1,30 @@
 function permutation_generator(value)
 {
-    
+    answerr=document.getElementById('answer');
+    var arr=value.split('');
+    answer={
+        list:''
+    }
+    answer_generator(answer, 0, arr, '');
+    answerr.innerHTML = answer.list;
+}
+function answer_generator(answer, index, arr, ans_string)
+{
+    if(ans_string.length==arr.length)
+    {
+        answer.list=answer.list+(ans_string+"<br>");
+        return;
+    }
+    for(var i=index; i<arr.length; i++)
+    {
+        swap(arr, i, index);
+        answer_generator(answer, index+1, arr, ans_string+arr[index]);
+        swap(arr, i, index);
+    }
+}
+function swap(arr, i, j)
+{
+    var temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
 }
--- Codeblocks/Javascripts/xyz.java
+++ Codeblocks/Javascripts/xyz.java
@@ -1,38 +1,38 @@
 package Javascripts;
+
 import java.io.*;
 import java.util.*;
-public class xyz
-{
-    public static void main(String []args)throws IOException
-    {
-        BufferedReader x=new BufferedReader(new InputStreamReader(System.in));
-        //String input=x.readLine();
 
-        String input="abcd";
+public class xyz {
+    static int count = 1;
 
-        char inp[]=input.toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+        // String input=x.readLine();
+
+        String input = "abc";
+
+        char inp[] = input.toCharArray();
         answer(inp, 0, "");
     }
-    static void answer(char[] arr, int index, String ans)
-    {
-        if(index==arr.length)
-        index=0;
 
-        if(ans.length()==arr.length)
-        {
-            System.out.println(ans);
+    static void answer(char[] arr, int index, String ans) {
+        if (ans.length() == arr.length) {
+            System.out.println(count + ": " + ans);
+            ++count;
             return;
         }
 
-        for(int i=index; i<arr.length; i++)
-        {
-            if(arr[i]!='0')
-            {
-                char temp=arr[i];
-                arr[i]='0';
-                answer(arr, i+1, ans+temp);
-                arr[i]=temp;
-            }
+        for (int i = index; i < arr.length; i++) {
+            swap(arr, i, index);
+            answer(arr, index + 1, ans + arr[index]);
+            swap(arr, i, index);
         }
+    }
+
+    static void swap(char[] arr, int a, int b) {
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
